4298f62f6cccd003d0f5551901f253dc54973dee
Brandon Kinman <brandon@kinmantech.org>
2018-06-28 22:01:04 -0700

    Initial commit

---
+++ arm_tools/dhparams.py
@@ -0,0 +1,15 @@
+# Definition of a link
+#  A link consists of 4 transformation parameters:
+#
+#     d: offset along previous z to the common normal
+# theta: angle about previous z, from old x to new x
+#     r: length of the common normal
+# alpha: angle about the common normal, from old z axis to new z axis
+
+# A Link has each of these properties.
+
+# An arm is essentially just a KinematicChain
+
+# A Kinematic chain is composed of links
+
+
